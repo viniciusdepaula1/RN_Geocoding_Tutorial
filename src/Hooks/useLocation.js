59a213f5d66fbb770e746bb3752fa2cf2470bfa3
Vinicius De Paula <viniciusdepaula199@gmail.com>
2020-11-05 09:54:42 -0300

    fix: verificando versão do sdk

--- src/Hooks/useLocation.js
+++ src/Hooks/useLocation.js
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react';
-import {requestMultiple, PERMISSIONS} from 'react-native-permissions';
+import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
 import Geolocation from 'react-native-geolocation-service';
+import { Platform } from 'react-native';
 
 export default () => {
   const [errorMsg, setErrorMsg] = useState(null);
   const [coords, setCoords] = useState(null);
 
   useEffect(() => {
-    (async function loadPosition(){
-      const result = await requestMultiple([PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION]).then(
-        (statuses) => {
-          const statusFine = statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
-          const statusBack = statuses[PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION];
+    (async function loadPosition() {
+      const result = await requestMultiple(
+        [
+          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+          PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION
+        ]).then(
+          (statuses) => {
+            const statusFine = statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
+            const statusBack = statuses[PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION];
 
-          if(statusFine == 'granted' && statusBack == 'granted'){
-            return true;
-          } else{
-            setErrorMsg('Usuário não aceitou solicitação de uso do GPS');
-          }
-        },
-      );
-      
-      if(result){
+            console.log(statusBack, statusFine);
+
+            if (Platform.Version < 29) {
+              if (statusFine == 'granted') {
+                return true;
+              } else {
+                setErrorMsg('Usuário não aceitou solicitação de uso do GPS');
+              }
+            }
+
+            if (statusFine == 'granted' && statusBack == 'granted') {
+              return true;
+            } else {
+              setErrorMsg('Usuário não aceitou solicitação de uso do GPS');
+            }
+          },
+        );
+
+      if (result) {
         await Geolocation.getCurrentPosition(
           ({ coords }) => {
             setCoords({
@@ -31,10 +46,10 @@ export default () => {
           }, (error) => {
             //console.log(error);
             setErrorMsg('Não foi possível obter a localização');
-          }, { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+          }, { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 } 
         )
       }
-        
+
     })()
   }, [])
 
